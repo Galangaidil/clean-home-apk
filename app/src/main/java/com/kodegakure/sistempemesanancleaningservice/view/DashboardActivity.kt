2047ab7ebea7388b05a32f5f27ac1e7ee93607a7
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-01-24 04:44:33 +0700

    Showing token in DashboardActivity.kt

--- app/src/main/java/com/kodegakure/sistempemesanancleaningservice/view/DashboardActivity.kt
+++ app/src/main/java/com/kodegakure/sistempemesanancleaningservice/view/DashboardActivity.kt
@@ -2,11 +2,16 @@ package com.kodegakure.sistempemesanancleaningservice.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.kodegakure.sistempemesanancleaningservice.R
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val token: String = intent.getStringExtra("token").toString()
+        val test = findViewById<TextView>(R.id.textView)
+        test.text = "Welcome, Token kamu adalah: \n $token"
     }
 }
